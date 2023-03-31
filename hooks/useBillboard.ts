02f5e5c58fb81ef94883befb6593f3b408d00257
Caspar2318 <108163945+Caspar2318@users.noreply.github.com>
2023-03-31 15:04:11 +1000

    movie lists

---
+++ hooks/useBillboard.ts
@@ -0,0 +1,10 @@
+import useSWR from "swr";
+import fetcher from "@/lib/fetcher";
+
+const useBillboard = () => {
+  const { data, error, isLoading } = useSWR("/api/random", fetcher);
+
+  return { data, error, isLoading };
+};
+
+export default useBillboard;
